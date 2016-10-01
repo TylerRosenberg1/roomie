759e8eb9859347a203aeb891f3e52cb1019ebd0e
Tyler Rosenberg <tylerrosenberg1@gmail.com>
2016-10-01 18:12:48 -0400

    added navbar

--- app.js
+++ app.js
@@ -18,5 +18,11 @@
       controllerAs: "VM",
       templateUrl: "views/user/show.html"
     })
+    .state("userEdit", {
+      url: "/user/:id/edit",
+      controller: "userEditController",
+      controllerAs: "VM",
+      templateUrl: "views/user/edit.html"
+    })
   }
 }())
---
+++ controllers/user/edit.js
@@ -0,0 +1,13 @@
+"use strict";
+
+(function() {
+  angular
+  .module("roomtrack")
+  .controller("userEditController", [
+    userEditControllerFunction
+  ])
+
+  function userEditControllerFunction() {
+
+  }
+}())
